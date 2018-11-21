f4422e53cfe456a3c95eaa5865a805e0560e3cdf
makozi <marizumakozi97@gmail.com>
2018-11-22 00:02:41 +0100

    updated .babelrc and server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 //server.js
+
+import express from 'express';
+const app= express()
+
+app.use(express.json())
+
+app.get('/',(req,res) =>{
+    return res.status(200).send({'message': 'Yay! The Contact API works '})
+})
+
+app.listen(5000)
+console.log('app running on port',5000);
